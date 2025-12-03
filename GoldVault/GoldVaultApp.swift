877b1b1c0884ccb4d84e752e55af65e1bf69de02
ludik <bakhanetssimon@gmail.com>
2025-12-03 10:44:19 +0300

    Initial Commit

---
+++ GoldVault/GoldVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoldVaultApp.swift
+//  GoldVault
+//
+//  Created by Simon Bakhanets on 03.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct GoldVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GoldVaultTests/GoldVaultTests.swift
@@ -0,0 +1,16 @@
+//
+//  GoldVaultTests.swift
+//  GoldVaultTests
+//
+//  Created by Simon Bakhanets on 03.12.2025.
+//
+
+import Testing
+
+struct GoldVaultTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
